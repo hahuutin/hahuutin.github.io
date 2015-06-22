f84f4eeeb72ac55c44d61087eec4e6814fa45f8d
TINHH <hahuutin1992@gmail.com>
2015-06-22 22:28:52 +0700

    complete intro comming soon for my portfolio

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	var widthWindow = $(window).width();
+	var heightWindow = $(window).height();
+
+});
+
+
+$(window).resize(function(){
+
+	var widthWindow = $(window).width();
+	var heightWindow = $(window).height();
+
+});
+
+// |++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// |
+// | Function for page load and resize
+// |++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
