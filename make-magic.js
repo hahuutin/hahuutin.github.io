d14918d9bdd03dfbf29bb807cd78d1ee1d90c39d
Ha Huu Tin <hahuutin1992@gmail.com>
2017-10-08 11:12:24 +0700

    update color schemes

--- make-magic.js
+++ make-magic.js
@@ -1,15 +1,8 @@
 window.onload = () => {
   // Random set of color + css variables
-  const themes = [
-    ['#FF8360', '#fff'],
-    ['#7DCE82', '#fff'],
-    ['#3CDBD3', '#fff'],
-    ['#00FFF5', '#fff'],
-  ];
+  const themes = ['#FF8360', '#7DCE82', '#3CDBD3', '#00FFF5', '#FFEA00', '#1565C0'];
   const index = Math.floor(Math.random() * themes.length);
-  const theme = themes[index];
-  document.body.style.setProperty('--main-color', theme[0]);
-  document.body.style.setProperty('--text-color', theme[1]);
+  document.body.style.setProperty('--main-color', themes[index]);
 
   // Animated element when enter the page
   document.body.classList.add('animated');
