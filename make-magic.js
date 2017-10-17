7406646182354993783443cbe23e2c3a3eefcc91
hahuutin <hahuutin1992@gmail.com>
2017-10-17 10:14:55 +0700

    update theme color

--- make-magic.js
+++ make-magic.js
@@ -1,6 +1,15 @@
 window.onload = () => {
   // Random set of color + css variables
-  const themes = ['#FF8360', '#7DCE82', '#3CDBD3', '#00FFF5', '#FFEA00', '#1565C0'];
+  const themes = [
+    '#f14357',
+    '#53a2e3',
+    '#f4ad49',
+    '#6757e2',
+    '#69de92',
+    '#e7654d',
+    '#f04576',
+    '#52c7eb'
+  ];
   const index = Math.floor(Math.random() * themes.length);
   document.body.style.setProperty('--main-color', themes[index]);
 
