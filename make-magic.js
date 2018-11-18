bd64f788ce88f94ffe2dc90fd39c17292ca9c9ec
hahuutin <hahuutin1992@gmail.com>
2018-11-18 08:21:13 +0700

    console log break 2 line

--- make-magic.js
+++ make-magic.js
@@ -1,5 +1,11 @@
-console.log("%c😁Hello World...!!! %cYou're curious about my site, right? Feel free to explore it! I hope you find something useful👏", "color: #fff; font-size: 30px; background: red", "color: #fff; font-size: 18px; background: blue");
-
+console.log(
+  "%c😁Hello World...!!!",
+  "color: #fff; font-size: 30px; background: red"
+);
+console.log(
+  "%cYou're curious about my site, right? Feel free to explore it! I hope you find something useful👏",
+  "color: #fff; font-size: 18px; background: blue"
+);
 
 window.onload = () => {
   // Random set of color + css variables
