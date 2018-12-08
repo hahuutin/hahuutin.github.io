358f5f15415376f168a43ca9ac1072e85cd67f77
hahuutin <hahuutin1992@gmail.com>
2018-12-09 03:21:25 +0700

    update style console.log and refactor file script

--- make-magic.js
+++ make-magic.js
@@ -1,27 +1,29 @@
+// Create random set of color themes
+const themes = [
+  '#f14357',
+  '#53a2e3',
+  '#f4ad49',
+  '#6757e2',
+  '#69de92',
+  '#e7654d',
+  '#f04576',
+  '#52c7eb',
+];
+const index = Math.floor(Math.random() * themes.length);
+const index_2 = index === 0 ? index + 1 : index - 1;
+
+// Change CSS Variables
+document.body.style.setProperty('--main-color', themes[index]);
+
+// Animated element when enter the page
+document.body.classList.add('animated');
+
+// Styled Console Log
 console.log(
-  "%c😁Hello World...!!!",
-  "color: #fff; font-size: 30px; background: red"
+  "%c✌Hello World",
+  `color: #fff; font-size: 30px; background-image: linear-gradient(to right, ${themes[index]}, ${themes[index_2]});`
 );
 console.log(
   "%cYou're curious about my site, right? Feel free to explore it! I hope you find something useful👏",
-  "color: #fff; font-size: 18px; background: blue"
+  `color: #fff; font-size: 18px; background-color: ${themes[index]}`
 );
-
-window.onload = () => {
-  // Random set of color + css variables
-  const themes = [
-    '#f14357',
-    '#53a2e3',
-    '#f4ad49',
-    '#6757e2',
-    '#69de92',
-    '#e7654d',
-    '#f04576',
-    '#52c7eb'
-  ];
-  const index = Math.floor(Math.random() * themes.length);
-  document.body.style.setProperty('--main-color', themes[index]);
-
-  // Animated element when enter the page
-  document.body.classList.add('animated');
-}
